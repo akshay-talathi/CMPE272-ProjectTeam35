3af0f81d910333fb2dff68199cf44f58521231b0
prashanty03 <prashant.m.yadav@gmail.com>
2014-12-08 01:07:55 -0800

    Committing Mobile Interface
    Commiting Sencha Touch code for Mobile interface

---
+++ Modile Interface/TL/build/temp/development/TasksListDemo/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/prashantyadav/Documents/TL/app.js
--- mysqldb.js
+++ mysqldb.js
@@ -3,10 +3,10 @@ var mysql = require('mysql');
 function getConnection(){
 	var connection = mysql.createConnection({        
 	    host: 'us-cdbr-iron-east-01.cleardb.net',
-	    user: 'bafb25895406ae',
-	    password : '2bf21561',
+	    user: 'bb020ceda88360',
+	    password : 'bc9c9db3',
 	    port : 3306, //port mysql
-	    database:'ad_582f6fae18af714'
+	    database:'ad_c10f80d933df6f1'
 	});
 	return connection;
 }
